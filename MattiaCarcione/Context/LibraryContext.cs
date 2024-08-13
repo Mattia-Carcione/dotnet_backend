385498a924ef6117ac73b8a1b48ff7457095541d
mattia carcione <mattia.carcione@alten.it>
2024-08-13 12:39:09 +0200

    add db context

---
+++ MattiaCarcione/Context/LibraryContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Entities;
+
+namespace Context;
+
+public class LibraryContext : DbContext
+{
+    public DbSet<Author> Authors {get; set;}
+    public DbSet<Book> Books {get; set;}
+    public DbSet<Category> Categories {get; set;}
+    public DbSet<Booking> Bookings {get; set;}
+    public DbSet<Editor> Editors {get; set;}
+    public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            {
+                throw new NullReferenceException();
+            }
+    }
+}
