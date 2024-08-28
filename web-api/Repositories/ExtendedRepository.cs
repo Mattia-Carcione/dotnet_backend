89ae2e3f63f38f35001d954307258b475ea9285c
mattia carcione <mattia.carcione@alten.it>
2024-08-28 17:20:52 +0200

    adding documentation

--- web-api/Repositories/ExtendedRepository.cs
+++ web-api/Repositories/ExtendedRepository.cs
@@ -11,6 +11,33 @@ public class ExtendedRepository<T> : GenericRepository<T>, IExtendedRepository<T
 {
     public ExtendedRepository(LibraryContext context) : base(context) { }
 
+    /// <summary>
+    /// Get a list of entities of type <typeparamref name="T"/> by passing a criteia <paramref name="expression"/>
+    /// and pagination metadata 
+    /// </summary>
+    /// 
+    /// <typeparam name="T">The type of the entities being required</typeparam>
+    /// <param name="pageNumber">the current of page number, starting from 1</param>
+    /// <param name="pageSize">the number of the item including in each page</param>
+    /// <param name="expression">A lambda expression rappresenting the filter criteria to apply <see cref="Func{T, TResult}"/></param>
+    /// <param name="queryLinq">A function <see cref="IQueryable{T}"/> that takes a LINQ operation (sorting must be provided)</param>
+    /// 
+    /// <returns>
+    /// An asynchronous task operation returning a tuple:
+    /// <list type="bullet">
+    /// 
+    /// <item>
+    /// <description>a <see cref="IEnumerable{T}"/> rappresenting the entities that match the criteria</description>
+    /// </item>
+    /// 
+    /// <item>
+    /// <description>a <see cref="PaginationMetadata"/> rappresenting an object that includes pagination details</description>
+    /// </item>
+    /// 
+    /// </list>
+    /// </returns>
+    /// 
+    /// <exception cref="Exception">If an error occurs during the execution of the task, the exception is captured and returned</exception>
     public async Task<(IEnumerable<T>, PaginationMetadata)> SearchByCriteriaAsync(int pageNumber, int pageSize, Expression<Func<T, bool>> expression, Func<IQueryable<T>, IQueryable<T>> queryLinq)
     {
         try
--- web-api/Repositories/GenericRepository.cs
+++ web-api/Repositories/GenericRepository.cs
@@ -13,6 +13,17 @@ public class GenericRepository<T> : IRepository<T>
 
     public GenericRepository(LibraryContext context) => _context = context;
 
+    /// <summary>
+    /// Add an entity <typeparamref name="T"/> into the db
+    /// </summary>
+    /// 
+    /// <typeparam name="T">The type of the entities being required</typeparam>
+    /// <param name="entity">the entity to add into the db</param>
+    /// 
+    /// <returns>
+    /// An asynchronous task operation adding the entity into the db
+    /// </returns>
+    /// <exception cref="Exception">If an error occurs during the execution of the task, the exception is captured and returned</exception>
     public async Task AddAsync(T entity)
     {
         try
@@ -26,6 +37,17 @@ public class GenericRepository<T> : IRepository<T>
         }
     }
 
+    /// <summary>
+    /// Update an entity <typeparamref name="T"/> that is already into the db
+    /// </summary>
+    /// 
+    /// <typeparam name="T">The type of the entities being required</typeparam>
+    /// <param name="entity">the entity to update into the db</param>
+    /// 
+    /// <returns>
+    /// A task operation updating the entity into the db
+    /// </returns>
+    /// <exception cref="Exception">If an error occurs during the execution of the task, the exception is captured and returned</exception>
     public void Update(T entity)
     {
         try
@@ -39,6 +61,17 @@ public class GenericRepository<T> : IRepository<T>
         }
     }
 
+    /// <summary>
+    /// Remove an entity <typeparamref name="T"/> from the db
+    /// </summary>
+    /// 
+    /// <typeparam name="T">The type of the entities being required</typeparam>
+    /// <param name="entity">the entity to remove from the db</param>
+    /// 
+    /// <returns>
+    /// A task operation removing the entity from the db
+    /// </returns>
+    /// <exception cref="Exception">If an error occurs during the execution of the task, the exception is captured and returned</exception>
     public void Delete(T entity)
     {
         try
@@ -52,14 +85,27 @@ public class GenericRepository<T> : IRepository<T>
         }
     }
 
-    public async Task<T?> GetAsync(int id, Func<IQueryable<T>, IQueryable<T>>? include = null)
+    /// <summary>
+    /// Get an entity <typeparamref name="T"/> by id, whether or not passing a criteria <paramref name="queryLinq">
+    /// </summary>
+    /// 
+    /// <typeparam name="T">The type of the entities being required</typeparam>
+    /// <param name="id">the entity id</param>
+    /// <param name="queryLinq">a function <see cref="IQueryable{T}"/> that takes a LINQ operation, such as including</param>
+    /// 
+    /// <returns>
+    /// An asynchronous task returning an entity <typeparamref name="T"/> 
+    /// </returns>
+    /// 
+    /// <exception cref="Exception">If an error occurs during the execution of the task, the exception is captured and returned</exception>
+    public async Task<T?> GetAsync(int id, Func<IQueryable<T>, IQueryable<T>>? queryLinq = null)
     {
         try
         {
             IQueryable<T> query = _context.Set<T>();
 
-            if(include != null)
-                query = include(query);
+            if(queryLinq != null)
+                query = queryLinq(query);
 
             return await query.FirstOrDefaultAsync(e => EF.Property<int>(e, "Id") == id);
         }
@@ -70,6 +116,32 @@ public class GenericRepository<T> : IRepository<T>
         }
     }
 
+    /// <summary>
+    /// Get a list of entities of type <typeparamref name="T"/> by passing a criteia <paramref name="expression"/>
+    /// and pagination metadata
+    /// </summary>
+    /// 
+    /// <typeparam name="T">The type of the entities being required</typeparam>
+    /// <param name="pageNumber">the current of page number, starting from 1</param>
+    /// <param name="pageSize">the number of the item including in each page</param>
+    /// <param name="queryLinq">A function <see cref="IQueryable{T}"/> that takes a LINQ operation (sorting must be provided)</param>
+    /// 
+    /// <returns>
+    /// An asynchronous task operation returning a tuple:
+    /// <list type="bullet">
+    /// 
+    /// <item>
+    /// <description>a <see cref="IEnumerable{T}"/> rappresenting the entities that match the criteria</description>
+    /// </item>
+    /// 
+    /// <item>
+    /// <description>a <see cref="PaginationMetadata"/> rappresenting an object that includes pagination details</description>
+    /// </item>
+    /// 
+    /// </list>
+    /// </returns>
+    /// 
+    /// <exception cref="Exception">If an error occurs during the execution of the task, the exception is captured and returned</exception>
     public async Task<(IEnumerable<T>, PaginationMetadata)> GetAllAsync(int pageNumber, int pageSize, Func<IQueryable<T>, IQueryable<T>> queryLinq)
     {
         try
@@ -96,6 +168,13 @@ public class GenericRepository<T> : IRepository<T>
         }
     }
 
+    /// <summary>
+    /// Saves all changes to the db
+    /// </summary>
+    /// 
+    /// <returns>
+    /// An asynchronous task operation saving the changes into the db
+    /// </returns>
     public async Task SaveChangesAsync()
     {
         try
